d7b95ab60963621acb99abafb7be22230df1db46
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-01-20 20:56:13 +0530

    [isogram] Pass tests for all words with alphabets only

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,7 +1,19 @@
 package isogram
 
+import "unicode"
+
 // IsIsogram tells if the given word or phrase is an isogram or not
 func IsIsogram(input string) bool {
-	isIsogram := true
-	return isIsogram
+	letterCount := make(map[rune]int)
+
+	for _, letter := range input {
+		lowerCaseLetter := unicode.ToLower(letter)
+		letterCount[lowerCaseLetter]++
+
+		if letterCount[lowerCaseLetter] > 1 {
+			return false
+		}
+	}
+
+	return true
 }
