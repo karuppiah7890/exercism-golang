24d4ca69acf66f09d94c57e05d4a3adc9a5b624a
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 21:05:57 +0530

    [leap] Invert all the if statements to use early returns

--- leap/leap.go
+++ leap/leap.go
@@ -4,14 +4,14 @@ const testVersion = 3
 
 // IsLeapYear takes year as parameter and returns true if it's leap year or false otherwise
 func IsLeapYear(year int) bool {
-	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
+	if year%4 != 0 {
+		return false
+	}
+	if year%100 != 0 {
 		return true
 	}
-	return false
+	if year%400 != 0 {
+		return false
+	}
+	return true
 }
