1df16671b086e58d6b2d5a048291d8939b0f7c2e
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 14:19:31 +0530

    [leap] Pass all tests

---
+++ leap/leap.go
@@ -0,0 +1,17 @@
+package leap
+
+const testVersion = 3
+
+// IsLeapYear takes year as parameter and returns true if it's leap year or false otherwise
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
