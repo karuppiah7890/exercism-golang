a606dc55840630b156e7c9e83c1de025d3cedafc
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 14:32:43 +0530

    [bob] Pass two more test cases

--- bob/bob.go
+++ bob/bob.go
@@ -2,7 +2,24 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import "unicode"
+
 // Hey returns bob's response to a remark
 func Hey(remark string) string {
+	shouting := true
+	for _, letter := range remark {
+		if !unicode.IsLetter(letter) {
+			continue
+		}
+
+		if unicode.IsLower(letter) {
+			shouting = false
+		}
+	}
+
+	if shouting {
+		return "Whoa, chill out!"
+	}
+
 	return "Whatever."
 }
