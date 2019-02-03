743e943d7245fb1328fe217af14edeaf4b222d22
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 14:28:31 +0530

    [bob] Pass on test case

---
+++ bob/bob.go
@@ -0,0 +1,8 @@
+// Package bob should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package bob
+
+// Hey returns bob's response to a remark
+func Hey(remark string) string {
+	return "Whatever."
+}
