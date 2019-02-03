c8f5822d50334b0d331804befa7e33bccb5c601f
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 09:51:32 +0530

    [difference-of-squares] Pass the tests

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,25 @@
+package diffsquares
+
+// SquareOfSum returns the square of the sum of first n natural numbers
+func SquareOfSum(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
+}
+
+// SumOfSquares returns the sum of the squares of first n natural numbers
+func SumOfSquares(n int) int {
+	sumOfSquares := 0
+	for i := 1; i <= n; i++ {
+		sumOfSquares += i * i
+	}
+	return sumOfSquares
+}
+
+// Difference returns the difference between the square of the sum and
+// the sum of the squares of the first N natural numbers
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
