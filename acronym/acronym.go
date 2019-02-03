1fbdd1e7574c45ff3817df167b2e2c0837c0afc0
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-03 22:04:37 +0530

    [acronym] Handle the hyphens in the phrases

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -10,11 +10,16 @@ import (
 
 // Abbreviate abbreviates a given sentence or phrase
 func Abbreviate(input string) string {
+	input = strings.Replace(input, "-", " ", -1)
 	words := strings.Split(input, " ")
 	abbreviation := ""
 
 	for _, word := range words {
-		abbreviation = fmt.Sprintf("%v%c", abbreviation, unicode.ToUpper(rune(word[0])))
+		if len(word) == 0 {
+			continue
+		}
+		letter := unicode.ToUpper(rune(word[0]))
+		abbreviation = fmt.Sprintf("%v%c", abbreviation, letter)
 	}
 
 	return abbreviation
