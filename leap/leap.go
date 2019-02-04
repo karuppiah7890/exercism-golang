13ab1acb17214c9f0f32543d087f9f13a3305ba8
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-04 23:03:13 +0530

    [leap] Remove unused variable

--- leap/leap.go
+++ leap/leap.go
@@ -1,7 +1,5 @@
 package leap
 
-const testVersion = 3
-
 // IsLeapYear takes year as parameter and returns true if it's leap year or false otherwise
 func IsLeapYear(year int) bool {
 	if year%4 != 0 {
